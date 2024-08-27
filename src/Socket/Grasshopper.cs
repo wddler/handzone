430327b9d7e1f7383e49468ea5eb50ec44afb6e2
luukgoossen <luuk.goossen@hotmail.com>
2024-08-27 13:02:50 +0200

    Add Meshes

--- src/Socket/Grasshopper.cs
+++ src/Socket/Grasshopper.cs
@@ -42,4 +42,58 @@ namespace Schema.Socket.Grasshopper
         [JsonProperty("run")]
         public bool Run { get; set; }
     }
+
+    /// <summary>
+    /// Sends a json payload of the meshes to Unity.
+    /// </summary>
+    public partial class GrasshopperMeshesIn
+    {
+        [JsonProperty("mesh1")]
+        public MeshData Mesh1 { get; set; }
+
+        [JsonProperty("mesh2")]
+        public MeshData Mesh2 { get; set; }
+
+        [JsonProperty("mesh3")]
+        public MeshData Mesh3 { get; set; }
+
+        [JsonProperty("mesh4")]
+        public MeshData Mesh4 { get; set; }
+
+        [JsonProperty("mesh5")]
+        public MeshData Mesh5 { get; set; }
+    }
+
+    public partial class MeshData
+    {
+        [JsonProperty("tris")]
+        public List<List<double>> Tris { get; set; }
+
+        [JsonProperty("vertices")]
+        public List<Vector3D> Vertices { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a 3D vector consisting of three components
+    /// </summary>
+    public partial class Vector3D
+    {
+        /// <summary>
+        /// X-axis
+        /// </summary>
+        [JsonProperty("x")]
+        public double X { get; set; }
+
+        /// <summary>
+        /// Y-axis
+        /// </summary>
+        [JsonProperty("y")]
+        public double Y { get; set; }
+
+        /// <summary>
+        /// Z-axis
+        /// </summary>
+        [JsonProperty("z")]
+        public double Z { get; set; }
+    }
 }
