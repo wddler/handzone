91c92bd597bb36e561b8ba972dcff167a8da1e72
Luuk Goossen <l.j.goossen@tudelft.nl>
2025-03-19 11:05:32 +0100

    Change URL

--- server/src/app/tutorials/page.tsx
+++ server/src/app/tutorials/page.tsx
@@ -27,8 +27,8 @@ export default async function Page() {
                 <div className='flex flex-1 shrink-0 grow flex-col'>
                     <div className='flex flex-col gap-4 py-16'>
                         <p>HANDZONe tutorials are currently hosted on DigiPedia. They introduce you to the basic features of robot anatomy, the different types of robot movements, waypoints, toolpaths, frames, and end effector setups. They also introduce how to create simple programs in PolyScope and more complex programs using the Robots plug-in in Rhino/Grasshopper for pick-and-place operations.</p>
-                        <p>After following these tutorials, you may practice with the virtual robot in HANDZONe or visit <a href="https://www.tudelft.nl/bk/onderzoek/bk-labs/lama" target="_blank" className="text-blue-400 hover:underline">LAMA</a> (Laboratory for Additive Manufacturing in Architecture) at TU Delft Faculty of Architecture and the Built Environment to develop hands-on skills.</p>
-                        <a href="https://digipedia.tudelft.nl/subject/ur5-tutorials/?tab=chapter-0" target="_blank" className="text-blue-400 hover:underline">Click here to access the tutorials on DigiPedia.</a>
+                        <p>After following these tutorials, you may practice with the virtual robot in HANDZONe or visit <a href="https://www.tudelft.nl/bk/onderzoek/bk-labs/lama" target="_blank" rel="noreferrer" className="text-blue-400 hover:underline">LAMA</a> (Laboratory for Additive Manufacturing in Architecture) at TU Delft Faculty of Architecture and the Built Environment to develop hands-on skills.</p>
+                        <a href="https://digipedia.tudelft.nl/subject/ur5-tutorials/?tab=chapter-0" target="_blank" rel="noreferrer" className="text-blue-400 hover:underline">Click here to access the tutorials on DigiPedia.</a>
                     </div>
                 </div>
             </main>
